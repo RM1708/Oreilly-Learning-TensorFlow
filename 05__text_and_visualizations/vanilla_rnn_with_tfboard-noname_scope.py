ae376e5ca0aeef050147591fc64f60b88163157b
RM <aaremnair@gmail.com>
2018-07-09 17:54:16 +0530

    05__text_and_visualizations/scan_example.py                         1. Added some print with formatting.
                    05__text_and_visualizations/vanilla_rnn_with_tfboard-noname_scope.py
                            1. Changed the mnist data directory to "/home/rm/tmp/data/"
    
                    05__text_and_visualizations/vanilla_rnn_with_tfboard.py
                            1. Changed log dir to LOG_DIR = "/home/rm/logs/RNN_with_summaries"

--- 05__text_and_visualizations/vanilla_rnn_with_tfboard-noname_scope.py
+++ 05__text_and_visualizations/vanilla_rnn_with_tfboard-noname_scope.py
@@ -30,29 +30,45 @@ y = tf.placeholder(tf.float32, shape=[None, num_classes], name='labels')
 
 # This helper function taken from official TensorFlow documentation,
 # simply add some ops that take care of logging summaries
+#def variable_summaries(var):
+#    with tf.name_scope('summaries'):
+#        mean = tf.reduce_mean(var)
+#        tf.summary.scalar('mean', mean)
+#        with tf.name_scope('stddev'):
+#            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+#        tf.summary.scalar('stddev', stddev)
+#        tf.summary.scalar('max', tf.reduce_max(var))
+#        tf.summary.scalar('min', tf.reduce_min(var))
+#        tf.summary.histogram('histogram', var)
+
 def variable_summaries(var):
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
-        tf.summary.histogram('histogram', var)
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
 
 
-# Weights and bias for input and hidden layer
-with tf.name_scope('rnn_weights'):
-    with tf.name_scope("W_x"):
-        Wx = tf.Variable(tf.zeros([element_size, hidden_layer_size]))
-        variable_summaries(Wx)
-    with tf.name_scope("W_h"):
-        Wh = tf.Variable(tf.zeros([hidden_layer_size, hidden_layer_size]))
-        variable_summaries(Wh)
-    with tf.name_scope("Bias"):
-        b_rnn = tf.Variable(tf.zeros([hidden_layer_size]))
-        variable_summaries(b_rnn)
+#Weights and bias for input and hidden layer
+#with tf.name_scope('rnn_weights'):
+#    with tf.name_scope("W_x"):
+#        Wx = tf.Variable(tf.zeros([element_size, hidden_layer_size]))
+#        variable_summaries(Wx)
+#    with tf.name_scope("W_h"):
+#        Wh = tf.Variable(tf.zeros([hidden_layer_size, hidden_layer_size]))
+#        variable_summaries(Wh)
+#    with tf.name_scope("Bias"):
+#        b_rnn = tf.Variable(tf.zeros([hidden_layer_size]))
+#        variable_summaries(b_rnn)
+
+Wx = tf.Variable(tf.zeros([element_size, hidden_layer_size]))
+variable_summaries(Wx)
+Wh = tf.Variable(tf.zeros([hidden_layer_size, hidden_layer_size]))
+variable_summaries(Wh)
+b_rnn = tf.Variable(tf.zeros([hidden_layer_size]))
+variable_summaries(b_rnn)
 
 
 def rnn_step(previous_hidden_state, x):
@@ -79,15 +95,22 @@ all_hidden_states = tf.scan(rnn_step,
 
 
 # Weights for output layers
-with tf.name_scope('linear_layer_weights') as scope:
-    with tf.name_scope("W_linear"):
-        Wl = tf.Variable(tf.truncated_normal([hidden_layer_size, num_classes],
-                                             mean=0, stddev=.01))
-        variable_summaries(Wl)
-    with tf.name_scope("Bias_linear"):
-        bl = tf.Variable(tf.truncated_normal([num_classes],
-                                             mean=0, stddev=.01))
-        variable_summaries(bl)
+#with tf.name_scope('linear_layer_weights') as scope:
+#    with tf.name_scope("W_linear"):
+#        Wl = tf.Variable(tf.truncated_normal([hidden_layer_size, num_classes],
+#                                             mean=0, stddev=.01))
+#        variable_summaries(Wl)
+#    with tf.name_scope("Bias_linear"):
+#        bl = tf.Variable(tf.truncated_normal([num_classes],
+#                                             mean=0, stddev=.01))
+#        variable_summaries(bl)
+
+Wl = tf.Variable(tf.truncated_normal([hidden_layer_size, num_classes],
+                                     mean=0, stddev=.01))
+variable_summaries(Wl)
+bl = tf.Variable(tf.truncated_normal([num_classes],
+                                     mean=0, stddev=.01))
+variable_summaries(bl)
 
 
 # Apply linear layer to state vector
@@ -96,26 +119,43 @@ def get_linear_layer(hidden_state):
     return tf.matmul(hidden_state, Wl) + bl
 
 
-with tf.name_scope('linear_layer_weights') as scope:
-    # Iterate across time, apply linear layer to all RNN outputs
-    all_outputs = tf.map_fn(get_linear_layer, all_hidden_states)
-    # Get Last output -- h_28
-    output = all_outputs[-1]
-    tf.summary.histogram('outputs', output)
+#with tf.name_scope('linear_layer_weights') as scope:
+#    # Iterate across time, apply linear layer to all RNN outputs
+#    all_outputs = tf.map_fn(get_linear_layer, all_hidden_states)
+#    # Get Last output -- h_28
+#    output = all_outputs[-1]
+#    tf.summary.histogram('outputs', output)
+#
+#with tf.name_scope('cross_entropy'):
+#    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=y))
+#    tf.summary.scalar('cross_entropy', cross_entropy)
+#
+#with tf.name_scope('train'):
+#    # Using RMSPropOptimizer
+#    train_step = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cross_entropy)
+#
+#with tf.name_scope('accuracy'):
+#    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(output, 1))
+#
+#    accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32)))*100
+#    tf.summary.scalar('accuracy', accuracy)
 
-with tf.name_scope('cross_entropy'):
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=y))
-    tf.summary.scalar('cross_entropy', cross_entropy)
+# Iterate across time, apply linear layer to all RNN outputs
+all_outputs = tf.map_fn(get_linear_layer, all_hidden_states)
+# Get Last output -- h_28
+output = all_outputs[-1]
+tf.summary.histogram('outputs', output)
 
-with tf.name_scope('train'):
-    # Using RMSPropOptimizer
-    train_step = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cross_entropy)
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=y))
+tf.summary.scalar('cross_entropy', cross_entropy)
 
-with tf.name_scope('accuracy'):
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(output, 1))
+# Using RMSPropOptimizer
+train_step = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cross_entropy)
 
-    accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32)))*100
-    tf.summary.scalar('accuracy', accuracy)
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(output, 1))
+
+accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32)))*100
+tf.summary.scalar('accuracy', accuracy)
 
 # Merge all the summaries
 merged = tf.summary.merge_all()
